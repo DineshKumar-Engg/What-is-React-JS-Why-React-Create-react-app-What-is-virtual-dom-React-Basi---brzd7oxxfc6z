5c14e239e09e7c5850d51b1d295648a50d7301e7
Dinesh kumar <dineshsoftwareengg@gmail.com>
2022-09-03 12:52:58 +0530

    final

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 // write code or Welcome component here
 
-export default function welcome(props){
-  let name=props.name;
-  return <>
-  <h1>Hey!{name}</h1>
+export default function Welcome(props){
+  // let name=props.name;
+  return <div>
+  <h1>Hey !{props.name}</h1>
   <h2>Welcome to Newton School.</h2>
-  </>
+  </div>
 }
